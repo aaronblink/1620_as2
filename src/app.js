0692be47ef57cebcc5ee9967dd7d4651e0706342
Aaron Arcalas <maaron.arcalas@gmail.com>
2022-04-09 15:42:16 -0700

    first commit with starter code

--- src/app.js
+++ src/app.js
@@ -5,3 +5,28 @@ const notes = [
     id: 1 
   }
 ]
+
+///create note taking area
+
+const createNote = document.querySelector("fa-solid fa-circle-plus")
+
+createNote.addEventListener(div => {
+    div.
+}
+  
+  
+  )
+
+//text are to write to
+
+//save button
+
+//cancel button
+
+///save a note
+
+///display notes in a side nav
+
+///read a note
+
+
