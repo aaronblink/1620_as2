b8aefb0e11c6526f4b9bf275b5f812928cdfaee3
Aaron Arcalas <maaron.arcalas@gmail.com>
2022-04-09 21:46:15 -0700

    Added function where clicking cancel button removes note taking area as well as the save and cancel buttons

--- src/app.js
+++ src/app.js
@@ -5,13 +5,20 @@ const notes = [
     id: 1 
   }
 ]
+//query-selectors
 const writeArea = document.querySelector(".write-note-area")
+const notesArea = document.querySelector(".side=note-menu")
+
+//text area to write to
 const textArea = `
 <textarea class="newNote" rows ="25" cols="55"></textarea>
 `
+//save button
 const saveButton = `<button class='saveButton' type='button'>
   Save
   </button>`
+
+//cancel button
 const cancelButton = `<button class='cancelButton' type='button'>
 Cancel
 </button>`
@@ -20,20 +27,22 @@ function createNote(){
   writeArea.insertAdjacentHTML('afterend', cancelButton)
   writeArea.insertAdjacentHTML('afterend', saveButton)
   writeArea.insertAdjacentHTML('afterend', textArea)
+  const cancelling = document.querySelector(".cancelButton")
+  cancelling.addEventListener("click", cancelNote);
 }
 
+function cancelNote(){
+  const cancelButton = document.querySelector(".cancelButton")
+  const saveButton = document.querySelector(".saveButton")
+  const textArea = document.querySelector(".newNote")
+  cancelButton.remove()
+  saveButton.remove()
+  textArea.remove()
+}
 
 ///create note taking area
 const noteButton = document.querySelector(".fa-solid.fa-circle-plus")
-
 noteButton.addEventListener("click", createNote);
-
-//text area to write to
-
-//save button
-
-//cancel button
-
 
 ///save a note
 
