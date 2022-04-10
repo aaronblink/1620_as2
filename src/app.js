5e64b59e09ba82adc648fb8d714b8953db3708d1
Aaron Arcalas <maaron.arcalas@gmail.com>
2022-04-09 20:23:36 -0700

    When plus button clicked, a note area is created includinga textarea, save button, and cancel button.

--- src/app.js
+++ src/app.js
@@ -5,23 +5,35 @@ const notes = [
     id: 1 
   }
 ]
+const writeArea = document.querySelector(".write-note-area")
+const textArea = `
+<textarea class="newNote" rows ="25" cols="55"></textarea>
+`
+const saveButton = `<button class='saveButton' type='button'>
+  Save
+  </button>`
+const cancelButton = `<button class='cancelButton' type='button'>
+Cancel
+</button>`
+
+function createNote(){
+  writeArea.insertAdjacentHTML('afterend', cancelButton)
+  writeArea.insertAdjacentHTML('afterend', saveButton)
+  writeArea.insertAdjacentHTML('afterend', textArea)
+}
+
 
 ///create note taking area
+const noteButton = document.querySelector(".fa-solid.fa-circle-plus")
 
-const createNote = document.querySelector("fa-solid fa-circle-plus")
+noteButton.addEventListener("click", createNote);
 
-createNote.addEventListener(div => {
-    div.
-}
-  
-  
-  )
-
-//text are to write to
+//text area to write to
 
 //save button
 
 //cancel button
+
 
 ///save a note
 
