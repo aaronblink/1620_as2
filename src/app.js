938e94bf140728151bb7a5f9f2186a1f7d360a0d
Aaron Arcalas <maaron.arcalas@gmail.com>
2022-04-11 19:17:50 -0700

    Edited dark mode toggle to use event objects

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const notes = [
-  { 
-    title: "Title", 
-    noteBody: "Test",
-    id: 1 
-  }
+  // { 
+  //   title: "Title", 
+  //   noteBody: "Test",
+  //   id: 1 
+  // }
 ]
 //query-selectors
 const writeArea = document.querySelector(".create-note-area")
 const notesArea = document.querySelector(".notes-list")
+const readArea = document.querySelector('.read-note-area')
 
 //text area to write to
 const textArea = `
@@ -21,6 +22,10 @@ const saveButton = `<button class='saveButton' type='button'>
 //cancel button
 const cancelButton = `<button class='cancelButton' type='button'>
 Cancel
+</button>`
+
+const closeButton = `<button class='closeButton' type='button'> 
+Close
 </button>`
 
 const textAreaButtons = `<div>${saveButton} ${cancelButton}</div>`
@@ -48,32 +53,51 @@ function cancelNote(){
 const noteButton = document.querySelector(".fa-solid.fa-circle-plus")
 noteButton.addEventListener("click", createNote);
 
-///save a note
+///display notes in a side nav
 function saveNote(){
   const textArea = document.querySelector(".newNote")
   const addedText = textArea.value.split("\n")
   const Title = addedText[0]
   const Body = addedText.slice(1)
   notes.push({title: Title, noteBody: Body, id:notes.length + 1})
-  const Titles = `<li>${Title}</li>`
+  const Titles = `<li class="${notes[notes.length - 1].id}">${Title}</li>`
   notesArea.insertAdjacentHTML('beforeend', Titles)
   cancelNote()
 }
 
+function closeDisplayNote() {
+  readArea.remove()
+  const closeButton = document.querySelector('.closeButton')
+  closeButton.remove()
+}
 
-///display notes in a side nav
+// ///read a note
+// notesArea.addEventListener("click", (evt) => {
+//   if (evt.target.className == notes[notes.length - 1].id) {
+//     readArea.insertAdjacentHTML('beforeend', `<h1>${notes[notes.length - 1].title}</h1>`)
+//     readArea.insertAdjacentHTML('afterend', closeButton)
+//     const closingBtn = document.querySelector(".closeButton")
+//     closingBtn.addEventListener("click", closeDisplayNote)
+//     console.log(closingBtn)
+//     for (const element of notes[notes.length -1].noteBody) {
+//       if (element == ''){  
+//       readArea.insertAdjacentHTML('beforeend', '<br>')
+//       } else {readArea.insertAdjacentHTML('beforeend', `<p>${element}</p>`)
+//                readArea.insertAdjacentHTML('beforeend', '<br>')  
+//     }}
+//   } 
+// })
+
 
 ///Bonus
-
 function toggleTheme() {
   const theme = document.querySelector(".main-container")
   const btnState = document.querySelector(".theme-toggle")
   btnState.addEventListener("click", (evt) => {
-    let checkbox = document.getElementsByClassName('theme-toggle')
-    if (checkbox.checked == true){
-      theme.classList.toggle("light-theme")
+    if (evt.target.checked == true){
+      theme.classList.add("dark-theme")
     } else {
-      theme.classList.toggle("dark-theme")
+      theme.classList.remove("dark-theme")
     }
   })
 }
